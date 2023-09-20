718a12e3d0a7d338fda068deddc1c170bb45ddfd
tarsel <michaelosimbo@yahoo.com>
2023-09-20 11:00:21 +0300

    Docker First Commit
    First Commit of Docker files into the repo for CI/CD pipeline.....

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,8 +1,11 @@
 const mongoose=require('mongoose')
 
+const url=process.env.DATABASE_URI
+
 const connectDB=async ()=>{
     try {
-        await mongoose.connect(process.env.DATABASE_URI,{
+        // await mongoose.connect(process.env.DATABASE_URI,{
+        await mongoose.connect('mongodb+srv://mongotut:testing123@cluster0.rkljslh.mongodb.net/CompanyDB?retryWrites=true&w=majority',{
             useUnifiedTopology:true,
             useNewUrlParser: true
         })
